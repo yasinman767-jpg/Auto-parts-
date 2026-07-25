6a3ed4797fa4f1665df94e854fa961b9cf782d4e
yasinman767-jpg <yasinman767@gmail.com>
2026-07-25 09:23:48 +0000

    Add Android release workflow and fix manifest/build config

--- extracted/react-native-app/android/app/src/main/java/com/autoparts/app/MainActivity.kt
+++ extracted/react-native-app/android/app/src/main/java/com/autoparts/app/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.autoparts.app
 
-import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-  }
-
-  override fun getMainComponentName(): String = "AutoPartsReactNative"
-
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
